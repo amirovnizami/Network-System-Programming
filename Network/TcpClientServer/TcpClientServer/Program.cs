a14c8a1387afb4cceded067115d3cfe744199eb9
Nizami <amirovnizami395@gmail.com>
2024-10-07 11:27:31 +0400

    ClientServer

---
+++ Network/TcpClientServer/TcpClientServer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+using System.Net;
+using System.Net.Sockets;
+
+var port = 27001;
+
+var ipAd = IPAddress.Parse("192.168.100.8");
+
+var endPoint = new IPEndPoint(ipAd, port);
+
+var listener = new TcpListener(endPoint);
+
+try
+{
+    listener.Start();
+
+    while (true)
+    {
+        var client = listener.AcceptTcpClient();
+
+        _ = Task.Run(() =>
+        {
+            Console.WriteLine($"{client.Client.RemoteEndPoint} connected");
+            var stream = client.GetStream();
+            var sr = new StreamReader(stream);
+            while (true)
+            {
+                var message = sr.ReadLine();
+                Console.WriteLine($"{client.Client.RemoteEndPoint} : {message}");
+            }
+        });
+    }
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message);
+}
---
+++ Network/TcpClientServer/TcpClientSide/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+var port = 27001;
+
+var IpAd = IPAddress.Parse("192.168.100.8");
+
+var endPoint = new IPEndPoint(IpAd, port);
+
+var client = new TcpClient();
+
+try
+{
+	client.Connect(endPoint);
+	if (client.Connected)
+	{
+		var sw = new StreamWriter(client.GetStream())
+		{
+			AutoFlush = true
+		};
+		while (true)
+		{
+			var path = @"C:\Users\amiro\Desktop\ConnectionString.txt";
+			var despath = @"C:\Users\amiro\Desktop\New.txt";
+            using (var fRead = new FileStream(path, FileMode.Open, FileAccess.Read))
+			{
+				using(var fWrite = new FileStream(despath, FileMode.OpenOrCreate, FileAccess.Write))
+				{
+                    var len = 1024;
+                    var bytes = new byte[len];
+                    do
+                    {
+                        len = fRead.Read(bytes, 0, len);
+						fWrite.Write(bytes, 0, len);
+                    }
+                    while (len > 0);
+                }
+				
+
+            }
+        }
+	}
+}
+catch (Exception ex)
+{
+
+	Console.WriteLine(ex.Message); ;
+}
